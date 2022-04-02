24bccd44c8473af3467a65e9fa591a9800232319
Andre Backman <abackman@student.hive.fi>
2022-04-02 15:05:22 +0300

    updated header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 16:07:36 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/01 17:14:09 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/02 13:14:29 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ typedef struct s_print
 	int		ret;
 	int		len;
 	int		check_neg;
+	int		save;
 	int		width;
 	int		precision;
 	int		hash;

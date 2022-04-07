e0faff4267f2a11e0bc0992947c20a98572d9065
Andre Backman <abackman@student.hive.fi>
2022-04-07 20:15:16 +0300

    updated float  conversion, handles -0.0

--- src/float.c
+++ src/float.c
@@ -6,13 +6,13 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 17:30:30 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/06 16:54:34 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/07 20:13:30 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static long double	ftoa_rounding(t_print *p, long double num)
+static long double	ftoa_rounding(int precision, long double num)
 {
 	long double	bank;
 	long double	tmp;
@@ -23,60 +23,64 @@ static long double	ftoa_rounding(t_print *p, long double num)
 	i = 0;
 	if (num < 0)
 		bank *= -1;
-	while (i < p->precision)
+	while (i < precision)
 	{
 		bank /= tmp;
 		i++;
 	}
-	tmp = num + bank;
-	return (tmp);
+	return (bank);
 }
 
-static char	*after_decimal(t_print *p, long double num, long long whole)
+static char	*decimal_str(int prec, long long tmp, char *buf)
 {
-	char				*buf;
-	long long			tmp;
-	double				dot;
-	int					prec;
-	int					len;
-	int					i;
-	static const double	power10[] = { 1, 10, 100, 1000, 10000, 100000, 1000000,\
-	10000000, 100000000, 1000000000 };
+	int	i;
 
 	i = 0;
-	if (p->precision == -1)
-		prec = 6;
-	else
-		prec = p->precision;
-	dot = (num - whole) * power10[prec];
-	tmp = (long long)dot;
-	len = ll_len(tmp);
-	//printf("* * FLOAT * *\nlen: %i\nprec: %i\ndot: %f\ntmp: %lli\n\n", len, prec, dot, tmp);
-	buf = (char *)malloc(prec + 2 * sizeof(char));
+	if (tmp < 0)
+		tmp *= -1;
 	while (tmp > 0)
 	{
-		buf[i] = tmp % 10 + '0';
+		buf[i++] = tmp % 10 + '0';
 		tmp /= 10;
-		i++;
 	}
-	while (len < prec)
+	while (i < prec)
 	{
-		buf[i] = '0';
-		i++;
-		len++;
+		buf[i++] = '0';
 	}
 	if (prec > 0)
 	{
 		buf[i++] = '.';
 		buf[i] = '\0';
+		str_reverse(buf);
 	}
 	else
 		buf[0] = '\0';
-	str_reverse(buf);
-	//buf = ft_itoa_base(tmp, 10, p);
 	return (buf);
 }
 
+static char	*after_decimal(t_print *p, char *nbr, long double num, \
+long long whole)
+{
+	char				*buf;
+	long long			tmp;
+	double				dot;
+	int					prec;
+
+	if (p->precision == -1)
+		prec = 6;
+	else
+		prec = p->precision;
+	num += ftoa_rounding(prec, num);
+	dot = init_dot(num, whole, prec);
+	tmp = (long long)dot;
+	buf = (char *)malloc(prec + 2 * sizeof(char));
+	if (!buf)
+		return (NULL);
+	buf = decimal_str(prec, tmp, buf);
+	buf = strjoin_pro(nbr, buf, 3);
+	return (buf);
+}
+/* 
 static char	*ftoa(t_print *p, long double num)
 {
 	char				*nbr;
@@ -84,31 +88,54 @@ static char	*ftoa(t_print *p, long double num)
 	char				*ret;
 	long long			whole;
 
-	num = ftoa_rounding(p, num);
 	whole = (long long)num;
 	nbr = ft_itoa_base(whole, 10, p);
 	dot = after_decimal(p, num, whole);
 	ret = strjoin_pro(nbr, dot, 2);
 	return (ret);
+} */
+static char	*float_signs(t_print *p, char *old, long double num)
+{
+	int		len;
+	char	tmp[5];
+	
+	len = 0;
+	if (1 / num < 0)
+		tmp[len++] = '-';
+	else if (p->space && !p->plus)
+		tmp[len++] = ' ';
+	else if (p->plus)
+		tmp[len++] = '+';
+	tmp[len] = '\0';
+	if (tmp[0] == '\0')
+		return (old);
+	else
+		return (strjoin_pro(tmp, old, 2));
 }
 
 int	pr_float(t_print *p)
 {
-	char		*new;
-	long double	num;
-	int			len;
-	
-	if (p->L)
-		num = (long double)va_arg(p->ap, long double);
-	else
-		num = (double)va_arg(p->ap, double);
-	new = ftoa(p, num);
-	new = insert_width(p, new, 1);
-	len = ft_strlen((const char *)new);
+	char			*nbr_str;
+	long double		num;
+	unsigned long	whole;
+	int				tmp;
+	long double		save;
+
+	tmp = 0;
+	num = float_length_mod(p);
+	save = num;
+	if (num < 0 || 1 / num < 0)
+		num *= -1;
+	whole = (unsigned long)num;
+	nbr_str = ft_utoa_base(whole, 10, p);
+	nbr_str = after_decimal(p, nbr_str, num, whole);
+	nbr_str = float_signs(p, nbr_str, save);
+	nbr_str = insert_width(p, nbr_str, 1);
+	tmp = ft_strlen((const char *)nbr_str);
 	if (p->str != NULL)
-		p->str = strjoin_pro(p->str, new, 1);
+		p->str = strjoin_pro(p->str, nbr_str, 1);
 	else if (p->str == NULL)
-		p->str = p_strnew((const char *)new, len);
-	ft_strdel(&new);
-	return (len);
+		p->str = p_strnew((const char *)nbr_str, tmp);
+	ft_strdel(&nbr_str);
+	return (tmp);
 }

01832047437f590d8cef149c3842ca1c9f852115
Andre Backman <abackman@student.hive.fi>
2022-04-07 20:17:21 +0300

    moved ftoa helper function

--- src/numbers.c
+++ src/numbers.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 11:15:34 by abackman          #+#    #+#             */
-/*   Updated: 2022/03/24 15:43:24 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/07 19:40:33 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,23 @@ int	ft_isdigit(int c)
 		return (1);
 	else
 		return (0);
+}
+
+double	init_dot(long double num, long long whole, int prec)
+{
+	double		power10[10];
+	double		dot;
+
+	power10[0] = 1;
+	power10[1] = 10;
+	power10[2] = 100;
+	power10[3] = 1000;
+	power10[4] = 10000;
+	power10[5] = 100000;
+	power10[6] = 1000000;
+	power10[7] = 10000000;
+	power10[8] = 100000000;
+	power10[9] = 1000000000;
+	dot = (num - whole) * power10[prec];
+	return (dot);
 }

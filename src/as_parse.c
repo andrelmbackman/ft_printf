e6a093b2750a5a815f5a6885802c07d8d05acd8f
Andre Backman <abackman@student.hive.fi>
2022-04-22 17:00:29 +0300

    norm check

--- src/as_parse.c
+++ src/as_parse.c
@@ -6,17 +6,45 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:23:59 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/04 20:30:19 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:00:16 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+static int	as_pr_char(t_print *print)
+{
+	char			*str;
+	unsigned char	c;
+
+	c = (unsigned char)va_arg(print->ap, int);
+	str = (char *)malloc(2 * sizeof(char));
+	if (!str)
+		return (-1);
+	str[0] = c;
+	str[1] = '\0';
+	if (print->width)
+	{
+		if (!ft_isprint(c))
+			str = insert_space_only(print, str);
+		else
+			str = insert_width(print, str, 1);
+	}
+	if (print->str != NULL)
+		print->str = strjoin_pro(print->str, str, 1);
+	else if (print->str == NULL)
+		print->str = str;
+	if (print->width)
+		return (print->width);
+	else
+		return (1);
+}
+
 char	*p_strnew(const char *format, int i)
 {
 	int		j;
 	char	*str;
-	
+
 	j = 0;
 	str = (char *)malloc(i * sizeof(char));
 	if (!str)
@@ -27,7 +55,6 @@ char	*p_strnew(const char *format, int i)
 		j++;
 	}
 	str[j] = '\0';
-	//printf("\nP_STRNEW: %s\n", str);
 	return (str);
 }
 
@@ -37,9 +64,8 @@ char	*p_strjoin(char *str, const char *format, int i)
 	int		j;
 	int		k;
 	int		l;
-	
+
 	j = ft_strlen(str) + i;
-	//printf("\nP_STRINGJOIN str: \"%s\" format: \"%s\" i: %i\n", str, format, i);
 	new = (char *)malloc(j + 1 * sizeof(char));
 	if (!new)
 		return (NULL);
@@ -53,7 +79,6 @@ char	*p_strjoin(char *str, const char *format, int i)
 	while (k < j)
 		new[k++] = format[l++];
 	new[k] = '\0';
-	//printf("stringjoin: \"%s\"\n", new);
 	ft_strdel(&str);
 	return (new);
 }
@@ -61,8 +86,10 @@ char	*p_strjoin(char *str, const char *format, int i)
 int	as_convert_yes(t_print *print, const char *format)
 {
 	int	x;
-	
+	int	ret;
+
 	x = 0;
+	ret = 0;
 	init_struct(print);
 	if (ft_strchr(FLAGS, format[print->i]))
 		get_field(format, print);
@@ -73,12 +100,14 @@ int	as_convert_yes(t_print *print, const char *format)
 	if (format[print->i] == SPECIFY[x])
 	{
 		print->conv = format[print->i];
-		//printf("CONVERT_YES SPECIFY[x] = %c %i p->i: %i\n", SPECIFY[x], x, print->i);
-		return (dispatch[x](print));
+		if (print->conv == 'c')
+			ret = as_pr_char(print);
+		else
+			ret = g_dispatch[x](print);
 	}
 	else
 		print->i--;
-	return (0);
+	return (ret);
 }
 
 int	as_convert_no(t_print *print, const char *format)
@@ -88,10 +117,10 @@ int	as_convert_no(t_print *print, const char *format)
 	i = 0;
 	while (format[i] != '\0' && format[i] != '%')
 		i++;
-	if (print->str)
+	if (print->str != NULL)
 		print->str = p_strjoin(print->str, format, i);
 	else
 		print->str = p_strnew(format, i);
-
+	print->ret += i;
 	return (i);
 }

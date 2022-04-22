68c942257d2aa3a2c6ceb464d852c2eabaef7938
Andre Backman <abackman@student.hive.fi>
2022-04-22 17:42:27 +0300

    removed comment

--- src/length.c
+++ src/length.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 13:28:13 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/07 17:39:39 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/22 16:32:06 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ unsigned long long	unsigned_length_mod(t_print *p)
 	unsigned long long i;
 	
 	i = 0;
-	if (p->l > 1)
-		i = (unsigned long long)va_arg(p->ap, unsigned long long);
-	else if (p->l == 1 || p->z)
+	if (p->l == 1 || p->z)
 		i = (unsigned long)va_arg(p->ap, unsigned long);
+	else if (p->l > 1)
+		i = (unsigned long long)va_arg(p->ap, unsigned long long);
 	else if (p->h == 1)
 		i = (unsigned short)va_arg(p->ap, int);
 	else if (p->h > 1)
@@ -53,7 +53,7 @@ long double	float_length_mod(t_print *p)
 	long double	num;
 
 	num = 0;
-	if (p->L)
+	if (p->cap_l)
 		num = (long double)va_arg(p->ap, long double);
 	else
 		num = (double)va_arg(p->ap, double);

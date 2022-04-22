2d30b8e1c8d9337c4dd62e93e0e11f89d774452f
Andre Backman <abackman@student.hive.fi>
2022-04-22 17:38:00 +0300

    norm check

--- src/flags.c
+++ src/flags.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 10:36:53 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/19 14:10:54 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:37:49 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static int	width_helper(const char *format, t_print *p)
 	int	width;
 
 	width = 0;
-	//printf("WIDTH HELPER1\n");
 	while (ft_strchr("0+- #*", format[p->i]))
 	{
 		if (format[p->i] == '0')
@@ -37,8 +36,6 @@ static int	width_helper(const char *format, t_print *p)
 			width = va_arg(p->ap, int);
 		p->i++;
 	}
-	//printf("* * * WIDTH HELPER2* * *\n");
-	//printf("zero: %i\nplus: %i\nminus: %i\nspace: %i\nhash: %i\nwidth: %i\n\n", p->zero, p->plus, p->minus, p->space, p->hash, width);
 	return (width);
 }
 
@@ -66,7 +63,6 @@ static void	get_width(const char *format, t_print *p)
 		p->width = new_width;
 	if (new_width && p->width && new_width < p->width)
 		p->width = new_width;
-	//printf("GET_WIDTH\nFORMAT CHAR: %c\ni: %i\nwidth: %i\nnew_width: %i\n", format[p->i], p->i, p->width, new_width);
 }
 
 static void	get_prec(const char *format, t_print *p, int save)
@@ -89,14 +85,11 @@ static void	get_prec(const char *format, t_print *p, int save)
 		save *= -1;
 	while (ft_isdigit(format[p->i]))
 		p->precision = (p->precision * 10) + (format[p->i++] - '0');
-	//while (format[p->i] && !ft_strchr(SPECIFY, format[p->i]))
-	//	p->i++;
 	p->p_save = save;
 	if (save && (!p->check_neg || !p->precision))
 		p->precision = save;
 	else if (p->check_neg && !save)
 		p->precision = 0;
-	//printf("\nGET_PREC\np->i: %i\np->precision: %i\nsave: %i\n", p->i, p->precision, save);
 }
 
 static void	get_length(const char *format, t_print *p)
@@ -108,7 +101,7 @@ static void	get_length(const char *format, t_print *p)
 		else if (format[p->i] == 'l')
 			p->l++;
 		else if (format[p->i] == 'L')
-			p->L++;
+			p->cap_l++;
 		else if (format[p->i] == 'z')
 			p->z++;
 		p->i++;
@@ -118,20 +111,16 @@ static void	get_length(const char *format, t_print *p)
 int	get_field(const char *format, t_print *print)
 {	
 	int	save;
-	
+
 	save = 0;
-	//printf("\nGET_FIELD1: str: \"%s\" char: \"%c\" PREC: %iEND\n", print->str, format[print->i], print->precision);
 	get_width(format, print);
 	while (format[print->i] && format[print->i] == '*')
 		print->i++;
-	//printf("GET_FIELD2: %c PREC: %i i: %i END\n", format[print->i], print->precision, print->i);
 	get_prec(format, print, save);
 	if (print->minus && !print->w_save && !print->p_save)
 		print->zero = 0;
 	else if (print->precision > 0 && print->zero)
 		print->zero = 0;
-	//printf("GET_FIELD3: %c PREC: %i i: %i END\n", format[print->i], print->precision, print->i);
 	get_length(format, print);
-	//printf("GET_FIELD4: %c PREC: %i i: %i END\n", format[print->i], print->precision, print->i);
 	return (0);
 }

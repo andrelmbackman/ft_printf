ddc8b7cdf06c0b90fa5dacd8194dd7e50158533a
Andre Backman <abackman@student.hive.fi>
2022-04-22 16:56:04 +0300

    norminette checked header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 16:07:36 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/21 15:15:31 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/22 16:48:50 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define BLACK "\033[0;30m"
 # define WHITE "\033[0;37m"
 # define FLAGS "0-+ #.*0123456789hlL"
-# define SPECIFY "%cspdibouxXf"
+# define SPECIFY "%cspdibouxXfb"
 # define ALL "0-+ #.*0123456789hlL%cspdibouxXf"
 
 /*
@@ -60,7 +60,7 @@ typedef struct s_print
 	int		minus;
 	int		h;
 	int		l;
-	int		L;
+	int		cap_l;
 	int		z;
 	char	conv;
 	char	*str;
@@ -70,32 +70,32 @@ typedef struct s_print
 ** Main functions
 */
 
-int	ft_printf(const char *format, ...);
-int	ft_dprintf(int fd, const char *format, ...);
-int	ft_asprintf(char **str, const char *format, ...);
+int					ft_printf(const char *format, ...);
+int					ft_dprintf(int fd, const char *format, ...);
+int					ft_asprintf(char **str, const char *format, ...);
 
 /*
 ** Print functions
 */
 
-int	pr_perc(t_print *print);
-int	pr_char(t_print *print);
-int	pr_str(t_print *print);
-int	pr_ptr(t_print *print);
-int	pr_num(t_print *print);
-int	pr_bin(t_print *print);
-int	pr_oct(t_print *print);
-int	pr_u(t_print *print);
-int	pr_hex(t_print *print);
-int	pr_float(t_print *print);
+int					pr_perc(t_print *print);
+int					pr_char(t_print *print);
+int					pr_str(t_print *print);
+int					pr_ptr(t_print *print);
+int					pr_num(t_print *print);
+int					pr_bin(t_print *print);
+int					pr_oct(t_print *print);
+int					pr_u(t_print *print);
+int					pr_hex(t_print *print);
+int					pr_float(t_print *print);
 
 /*
 ** Function pointer and dispatch table
 */
 
-typedef int	(*t_able)(t_print *print);
+typedef int			(*t_able)(t_print *print);
 
-static const t_able dispatch[13] = {
+static const t_able	g_dispatch[14] = {
 	pr_perc,
 	pr_char,
 	pr_str,
@@ -108,6 +108,7 @@ static const t_able dispatch[13] = {
 	pr_hex,
 	pr_hex,
 	pr_float,
+	pr_bin,
 	NULL
 };
 
@@ -115,50 +116,56 @@ static const t_able dispatch[13] = {
 ** ft_asprintf functions
 */
 
-int		as_convert_yes(t_print *print, const char *format);
-int		as_convert_no(t_print *print, const char *format);
+int					as_convert_yes(t_print *print, const char *format);
+int					as_convert_no(t_print *print, const char *format);
 
 /*
 ** ft_printf and ft_dprintf functions
 */
 
-int		convert_yes(t_print *print, const char *format);
-int		convert_no(t_print *print, const char *format);
+int					convert_yes(t_print *print, const char *format);
+int					convert_no(t_print *print, const char *format);
 
 /*
 ** Other functions
 */
 
-void	free_struct(t_print *print);
-void	init_struct(t_print *print);
-void	str_reverse(char *str);
-void	ft_strdel(char **as);
-int		ft_strlen(const char *str);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isprint(int c);
-int		ll_len(long long n);
-int		get_field(const char *format, t_print *print);
-char	*ft_strcpy(char *dst, const char *src);
-char	*zerostr(void);
-char	*zeropad(char *str, int total, long long num);
-char	*strnull(t_print *p, char *str);
-char	*p_strnew(const char *format, int i);
-char	*p_strjoin(char *str, const char *format, int i);
-char	*strjoin_pro(char *str1, char *str2, int n);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-char	*ft_itoa_base(long long num, int base, t_print *print);
-char	*ft_utoa_base(unsigned long long num, int base, t_print *print);
-char	*fl_width(t_print *p, char *str);
-char	*insert_width(t_print *p, char *tmp, int free);
-char	*insert_space_only(t_print *p, char *str);
-double	init_dot(long double num, long long whole, int prec);
-long long signed_length_mod(t_print *p);
+int					ft_strlen(const char *str);
+int					ft_strcmp(const char *s1, const char *s2);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isprint(int c);
+int					ll_len(long long n);
+int					get_field(const char *format, t_print *print);
+void				free_struct(t_print *print);
+void				init_struct(t_print *print);
+void				str_reverse(char *str);
+void				ft_strdel(char **as);
+char				*ft_strcpy(char *dst, const char *src);
+char				*zerostr(void);
+char				*zeropad(char *str, int total, long long num);
+char				*strnull(t_print *p, char *str);
+char				*p_strnew(const char *format, int i);
+char				*p_strjoin(char *str, const char *format, int i);
+char				*strjoin_pro(char *str1, char *str2, int n);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strdup(const char *s1);
+char				*ft_itoa_base(long long num, int base, t_print *print);
+char				*ft_utoa_base(unsigned long long num, int base, \
+t_print *print);
+char				*fl_width(t_print *p, char *str);
+char				*insert_width(t_print *p, char *tmp, int free);
+char				*insert_space_only(t_print *p, char *str);
+double				init_dot(long double num, long long whole, int prec);
+long double			ftoa_rounding(int precision, long double num);
+
+/*
+** Length modifiers
+*/
+
 unsigned long long	unsigned_length_mod(t_print *p);
-long double	float_length_mod(t_print *p);
-long double	ftoa_rounding(int precision, long double num);
+long double			float_length_mod(t_print *p);
+long long			signed_length_mod(t_print *p);
 
 #endif

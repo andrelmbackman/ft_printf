703c1a624601ef2b947a7bd8bb25712128dab298
Andre Backman <abackman@student.hive.fi>
2022-04-22 17:38:43 +0300

    norm check

--- src/float.c
+++ src/float.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 17:30:30 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/21 16:04:48 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:38:31 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static char	*decimal_str(t_print *p, int prec, long long tmp, char *buf)
 	int	i;
 
 	i = 0;
-	//printf("\nDECIMAL_STR\nprec: %i\ntmp: %lli\n", prec, tmp);
 	if (tmp < 0)
 		tmp *= -1;
 	while (tmp > 0)
@@ -27,7 +26,6 @@ static char	*decimal_str(t_print *p, int prec, long long tmp, char *buf)
 	}
 	while (i < prec)
 		buf[i++] = '0';
-	//printf("\nDECIMAL STR\np->hash: %i\np->precision: %i\n\n", p->hash, p->precision);
 	if (p->precision != 0 || p->hash)
 		buf[i++] = '.';
 	if (prec > 0)
@@ -55,7 +53,6 @@ long long whole)
 		prec = p->precision;
 	dot = init_dot(num, whole, prec);
 	tmp = (long long)dot;
-	//printf("\n* * * AFTER DECIMAL * * *\ndot: %f\n(long long)dot %lli\n", dot, tmp);
 	buf = (char *)malloc(prec + 2 * sizeof(char));
 	if (!buf)
 		return (NULL);
@@ -68,7 +65,7 @@ static char	*float_signs(t_print *p, char *old, long double num)
 {
 	int		len;
 	char	tmp[5];
-	
+
 	len = 0;
 	if (1 / num < 0)
 		tmp[len++] = '-';
@@ -99,11 +96,9 @@ int	pr_float(t_print *p)
 	if (p->precision != 0 || num > 0.999999)
 		num = ftoa_rounding(p->precision, num);
 	whole = (unsigned long)num;
-	//printf("\n* * * 6 * * *\n whole: %lu\nnum: %Lf\n", whole, save);
 	nbr_str = ft_utoa_base(whole, 10, p);
 	nbr_str = after_decimal(p, nbr_str, num, whole);
 	nbr_str = float_signs(p, nbr_str, save);
-	//nbr_str = insert_width(p, nbr_str, 1);
 	nbr_str = fl_width(p, nbr_str);
 	ret = ft_strlen((const char *)nbr_str);
 	if (p->str != NULL)

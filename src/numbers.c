11a65d3af33b50b3681dd4cee42d0bc1fe7936f9
Andre Backman <abackman@student.hive.fi>
2022-04-25 18:04:02 +0300

    added comments

--- src/numbers.c
+++ src/numbers.c
@@ -6,19 +6,15 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 11:15:34 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/25 12:34:16 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/25 17:56:19 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
-}
+/*
+** Helper functions for float conversion.
+*/
 
 double	init_dot(long double num, long long whole, int prec)
 {
@@ -33,10 +29,10 @@ double	init_dot(long double num, long long whole, int prec)
 	return (dot);
 }
 
-static char	*swap_space(char *new)
+static char	*fl_swap_space(char *new)
 {
 	int		i;
-	
+
 	i = 0;
 	while (new[i])
 	{
@@ -51,10 +47,10 @@ static char	*swap_space(char *new)
 	return (new);
 }
 
-static char	*pad_right(t_print *p, char *new, char *tmp, int start)
+static char	*fl_pad_right(t_print *p, char *new, char *tmp, int start)
 {
 	int		x;
-	
+
 	x = ft_strlen(tmp);
 	while (start < x)
 	{
@@ -67,12 +63,12 @@ static char	*pad_right(t_print *p, char *new, char *tmp, int start)
 	return (new);
 }
 
-static char	*pad_left(t_print *p, char *new, char *tmp, int start)
+static char	*fl_pad_left(t_print *p, char *new, char *tmp, int start)
 {
 	int		x;
-	int 	save;
+	int		save;
 	char	pad;
-	
+
 	pad = '0';
 	x = ft_strlen(tmp);
 	new = ft_strcpy(new, (const char *)tmp);
@@ -89,7 +85,7 @@ static char	*pad_left(t_print *p, char *new, char *tmp, int start)
 		new[start++] = tmp[x++];
 	new[start] = '\0';
 	if (p->space)
-		new = swap_space(new);
+		new = fl_swap_space(new);
 	return (new);
 }
 
@@ -107,9 +103,9 @@ char	*fl_width(t_print *p, char *str)
 	if (!new)
 		return (str);
 	if (p->minus)
-		new = pad_right(p, new, str, 0);
+		new = fl_pad_right(p, new, str, 0);
 	else
-		new = pad_left(p, new, str, i);
+		new = fl_pad_left(p, new, str, i);
 	ft_strdel(&str);
 	return (new);
 }

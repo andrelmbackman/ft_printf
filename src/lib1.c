423c1abbdd6e81357dc85daa9b3165ec9b94630f
Andre Backman <abackman@student.hive.fi>
2022-04-25 12:42:19 +0300

    norm check

--- src/lib1.c
+++ src/lib1.c
@@ -6,12 +6,11 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 16:00:34 by abackman          #+#    #+#             */
-/*   Updated: 2022/04/04 12:23:23 by abackman         ###   ########.fr       */
+/*   Updated: 2022/04/25 12:33:03 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
 
 void	ft_strdel(char **as)
 {
@@ -29,9 +28,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	i = 0;
 	while (s1[i] == s2[i] && (s1[i] != '\0') && (s2[i] != '\0'))
-	{	
 		i++;
-	}
 	c1 = (unsigned char)s1[i];
 	c2 = (unsigned char)s2[i];
 	return (c1 - c2);
